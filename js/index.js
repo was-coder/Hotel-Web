f2168608b7212f0d789b15bc0fb9134314359db8
was-coder <wasleadwears4u2c@gmail.com>
2020-06-21 00:59:17 +0400

    REFACTOR: CHANGED MANY FEATURES FROM THE CODE
    The code is refactored to style mobile first before styling desktop and the hamburger was changed entirely to entertain more javascript and the bootstrap cdn was added to the code to make the carousel work effectively

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
-function openBurger() {
-    document.getElementById("myNav").style.width = "100%";
-}
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
 
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu () {
+    if (!showMenu) {
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        navItems.forEach(item => item.classList.add('open'));
+        showMenu = true;
+    }
+    else {
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        navItems.forEach(item => item.classList.remove('open'));
+        showMenu = false;
+    }
+};
