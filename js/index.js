f4e5ae8f03d33eedd2dbd16e330ac6219674dc66
was-coder <wasleadwears4u2c@gmail.com>
2020-07-21 21:14:55 +0400

    FEAT:ADDED THE BOOK NOW FEATURE
    I added the book now feature and made it responsive for all mobile

--- js/index.js
+++ js/index.js
@@ -27,3 +27,52 @@ function toggleMenu () {
 };
 /*End of JS for button toggling*/
 
+const form = document.getElementById("form");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    getInputs();
+});
+
+function getInputs() {
+    const nameValue = name.value.trim();
+    const emailValue = email.value.trim();
+
+    if (nameValue === "") {
+        setFail(name, "Name cannot be blank");
+    }
+    else {
+        setSuccess(name);
+    }
+
+    if (emailValue === "") {
+        setFail(email, "Email cannot be blank");
+    }
+    else if (!emailCheck(emailValue)) {
+        setFail(email, "Email is not valid");
+    }
+    else {
+        setSuccess(email);
+    }
+}
+
+function setFail(input, message) {
+    const control = input.parentElement;
+    const small = control.querySelector('small');
+
+    small.innerText = message;
+    control.className = "control fail";
+}
+
+function setSuccess(input) {
+    const control = input.parentElement;
+    control.className = "control success";
+}
+
+function emailCheck(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
